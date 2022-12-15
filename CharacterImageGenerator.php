eef17977b6e9ec206957286184f85eb9852daca7
unconv <unconventionalcoding@gmail.com>
2022-12-15 06:43:43 +0200

    switched to using custom trim function and moved it to Image class

--- CharacterImageGenerator.php
+++ CharacterImageGenerator.php
@@ -16,18 +16,8 @@ class CharacterImageGenerator
         // add letter to image
         imagettftext( $im, $size-1, 0, 0, $size-1, $black, $font, $letter );
 
-        return $this->trim( $im );
-    }
+        $im = Image::trim( $im );
 
-    public function trim( GdImage $im ): GdImage {
-        // get blob of image
-        $image = Image::get_blob( $im );
-
-        // trim white borders from image
-        $im2 = new Imagick();
-        $im2->readImageBlob( $image );
-        $im2->trimImage( 0 );
-
-        return imagecreatefromstring( (string) $im2 );
+        return $im;
     }
 }
--- Image.php
+++ Image.php
@@ -1,6 +1,8 @@
 <?php
 class Image
 {
+    public const TRIM_COLOR_THRESHOLD = 180;
+
     public static function save_to_file( GdImage $image, string $filename ) {
         $image = Image::get_blob( $image );
         return file_put_contents( $filename, $image );
@@ -17,5 +19,49 @@ class Image
         $color_index = imagecolorat( $image, $x, $y );
         $colors = imagecolorsforindex( $image, $color_index );
         return $colors[$color];
+    }
+
+    public static function trim( GdImage $image ): GdImage {
+        $width = imagesx( $image );
+        $height = imagesy( $image );
+
+        $start_x = null;
+        $start_y = null;
+        $end_x = null;
+        $end_y = null;
+
+        for( $y = 0; $y < $height; $y++ ) {
+            for( $x = 0; $x < $width; $x++ ) {
+                $color = Image::get_color( $image, $x, $y );
+                if( $color <= Image::TRIM_COLOR_THRESHOLD ) {
+                    if( $start_y === null ) {
+                        $start_y = $y;
+                    }
+
+                    $end_y = $y+1;
+                }
+            }
+        }
+
+        for( $x = 0; $x < $width; $x++ ) {
+            for( $y = 0; $y < $height; $y++ ) {
+                $color = Image::get_color( $image, $x, $y );
+                if( $color <= Image::TRIM_COLOR_THRESHOLD ) {
+                    if( $start_x === null ) {
+                        $start_x = $x;
+                    }
+
+                    $end_x = $x+1;
+                }
+            }
+        }
+
+        return imagecrop( $image, [
+            "x" => $start_x,
+            "y" => $start_y,
+            "width" => $end_x - $start_x,
+            "height" => $end_y - $start_y
+        ] );
+
     }
 }
