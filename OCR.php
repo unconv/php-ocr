a6baf96d75d5bdd2debfee6d8a20366753a21165
unconv <unconventionalcoding@gmail.com>
2022-12-15 08:51:32 +0200

    modified OCR::read() method so that it reads text using all fonts in the fonts folder and returns the text that got the best score

--- OCR.php
+++ OCR.php
@@ -1,13 +1,39 @@
 <?php
 class OCR
 {
-    public function read( string $filename, string $font_filename ) {
+    public function read( string $filename ) {
+        $fonts = glob( "fonts/*.ttf" );
+
+        $best_font = null;
+        $best_score = 0;
+
+        foreach( $fonts as $font_filename ) {
+            $data = $this->read_by_font( $filename, $font_filename );
+
+            if( $data['score'] > $best_score ) {
+                $best_score = $data['score'];
+                $best_font = basename( $font_filename );
+                $best_text = $data['output'];
+            }
+        }
+
+        return [
+            "score" => $best_score,
+            "font" => $best_font,
+            "text" => $best_text,
+        ];
+    }
+
+    public function read_by_font( string $filename, string $font_filename ): array {
         $read_this = imagecreatefrompng( $filename );
 
         $reader = new TextReader();
         $lines = $reader->lines_to_images( $read_this );
 
         $output = "";
+
+        $score = 0;
+        $letter_count = 0;
 
         foreach( $lines as $line ) {
             $letters = $reader->line_to_letters( $line );
@@ -16,15 +42,23 @@ class OCR
                 $space = $letter['space'];
                 $letter_data = new LetterData( $letter_image );
                 $which = OCR::which( $letter_data, $font_filename );
-                $output .= $space.$which;
+                $output .= $space.$which['letter'];
+
+                $score += $which['score'];
+                $letter_count++;
             }
             $output .= PHP_EOL;
         }
 
-        return rtrim( $output );
+        $output = rtrim( $output );
+
+        return [
+            "output" => $output,
+            "score" => $score / $letter_count,
+        ];
     }
 
-    public static function which( LetterData $letter, string $font_filename ): string|null {
+    public static function which( LetterData $letter, string $font_filename ): array {
         $refernce_data = LetterData::generate_reference_material( $font_filename );
 
         $best_guess = null;
@@ -38,6 +72,9 @@ class OCR
             }
         }
 
-        return $best_guess;
+        return [
+            "letter" => $best_guess,
+            "score" => $best_score,
+        ];
     }
 }
